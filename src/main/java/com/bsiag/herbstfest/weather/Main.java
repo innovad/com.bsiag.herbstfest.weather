471b774dc8a14e25daae27b81508ca53c9700f90
Adrian Moser <amo@bsiag.com>
2015-02-25 15:28:37 +0100

    default WOEID changed to Munich

--- src/main/java/com/bsiag/herbstfest/weather/Main.java
+++ src/main/java/com/bsiag/herbstfest/weather/Main.java
@@ -16,7 +16,8 @@ public class Main {
 		// Change the default location (woeid), build and run again.
 
 		// Read the WOEID Code from the Command-line
-		String woeid = "781712";
+		String woeid = "676757"; // München
+		// String woeid = "781712"; // Baden
 		// String woeid = "12478530";
 		try {
 			woeid = args[0];
